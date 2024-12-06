6b200208ce942b8fdc596ea39584957ec8d2e1ec
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2024-12-06 13:41:26 +0200

    added: script to count and save the number of US and non-US billionaires to a text file

---
+++ scripts/how-many-us-citizens.py
@@ -0,0 +1,34 @@
+import os 
+from pymongo import MongoClient
+
+def how_many_us_citizenship():
+    """Returns the number of billionaires with US citizenship"""
+    
+    client = MongoClient(os.getenv('DATABASE_CONNECTION'))
+    db = client.forbes_billionaires
+    collection = db.billionaires
+    
+    american_citizens = 0 
+    non_american_citizens = 0
+    
+    billionaires = collection.find({})
+    
+    for billionaire in billionaires: 
+        citizenship = billionaire.get('Citizenship')
+        if 'United States' in citizenship:
+            american_citizens += 1
+        else:
+            non_american_citizens += 1
+            
+    if not os.path.exists('auto'):
+        os.makedirs('auto')
+        
+    result = f"American Citizens: {american_citizens}\nNon-American Citizens: {non_american_citizens}"
+    
+    with open('auto/us_citizenship.txt', 'w') as f:
+        f.write(result)
+        
+    print(f"Data saved to auto/us_citizenship.txt")
+    
+if __name__ == "__main__":
+    how_many_us_citizenship()

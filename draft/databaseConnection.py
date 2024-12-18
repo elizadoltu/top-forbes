830e7609eaae4715b330f1fb5790fa95300bcc05
Doltu Teodora - Eliza <66266646+elizadoltu@users.noreply.github.com>
2024-12-18 09:48:02 +0200

    Delete draft directory

--- draft/databaseConnection.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-def connection():
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.forbes_billionaires 
-    collection = db.billionaires
